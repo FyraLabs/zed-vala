5e7e906c8d410b4018588814c4e525c3d6c4e57a
Lleyton Gray <lleyton@fyralabs.com>
2024-07-15 17:07:05 -0700

    feat: VLS support

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ https://github.com/vala-lang/vala-vscode
 https://github.com/lv37/zed-v
 https://github.com/zed-industries/zed/tree/main/extensions/csharp
 https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries/vala
+https://github.com/zed-industries/zed/tree/main/extensions/deno
---
+++ src/vala.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use zed::lsp::CompletionKind;
+use zed::{serde_json, CodeLabel, CodeLabelSpan, LanguageServerId};
+use zed_extension_api::settings::LspSettings;
+use zed_extension_api::{self as zed, Result};
+
+struct ValaExtension;
+
+impl ValaExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<String> {
+        if let Some(path) = worktree.which("vala-language-server") {
+            return Ok(path);
+        }
+
+        Err(format!("vala-language-server not installed"))
+
+        // TODO: auto-download VLS from a precompiled binary; the official github repo currently doesn't have builds :/
+    }
+}
+
+impl zed::Extension for ValaExtension {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<zed::Command> {
+        Ok(zed::Command {
+            command: self.language_server_binary_path(language_server_id, worktree)?,
+            args: vec![],
+            env: Default::default(),
+        })
+    }
+}
+
+zed::register_extension!(ValaExtension);
